8c9d21b6567eacd8815dc7ccf105e96088b1ea3f
杨安琪 <yangllzz01@163.com>
2016-03-27 23:30:00 +0800

    Click "recommend/shake/route" to chage to different fragments.

--- app/src/main/java/com/tjsse/egteam/entertainment_guider/MainActivity.java
+++ app/src/main/java/com/tjsse/egteam/entertainment_guider/MainActivity.java
@@ -1,13 +1,75 @@
 package com.tjsse.egteam.entertainment_guider;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements OnClickListener{
+    private Button btnRecommend;
+    private Button btnShake;
+    private Button btnRoute;
+
+    private Fragment recommendFragment;
+    private Fragment shakeFragment;
+    private Fragment routeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRecommend=(Button)findViewById(R.id.button_recommend);
+        btnShake=(Button)findViewById(R.id.button_shake);
+        btnRoute=(Button)findViewById(R.id.button_route);
+
+        btnRecommend.setOnClickListener(this);
+        btnShake.setOnClickListener(this);
+        btnRoute.setOnClickListener(this);
+
+        setDefaultFragment();
     }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fm=getFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+
+        switch(v.getId()){
+            case R.id.button_recommend:
+                if(recommendFragment==null){
+                    recommendFragment=new RecommendFragment();
+                }
+                ft.replace(R.id.id_content,recommendFragment);
+                break;
+            case R.id.button_shake:
+                if(shakeFragment==null){
+                    shakeFragment=new ShakeFragment();
+                }
+                ft.replace(R.id.id_content,shakeFragment);
+                break;
+            case R.id.button_route:
+                if(routeFragment==null){
+                    routeFragment=new RouteFragment();
+                }
+                ft.replace(R.id.id_content,routeFragment);
+                break;
+        }
+
+        ft.commit();
+
+    }
+
+    private void setDefaultFragment() {
+        FragmentManager fm=getFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+        RecommendFragment recommendFragment=new RecommendFragment();
+        ft.replace(R.id.id_content, recommendFragment);
+        ft.commit();
+    }
+
 }
---
+++ app/src/main/java/com/tjsse/egteam/entertainment_guider/RouteFragment.java
@@ -0,0 +1,17 @@
+package com.tjsse.egteam.entertainment_guider;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Angel on 16/3/27.
+ */
+public class RouteFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState){
+        return inflater.inflate(R.layout.fragment_route,container,false);
+    }
+}
