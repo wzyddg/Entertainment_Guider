f2076b69fa5e55530bcc0372e4dde275c2c9779f
杨安琪 <yangllzz01@163.com>
2016-03-28 17:01:12 +0800

    Update ShakeFragment

--- app/src/main/java/com/tjsse/egteam/entertainment_guider/ShakeFragment.java
+++ app/src/main/java/com/tjsse/egteam/entertainment_guider/ShakeFragment.java
@@ -12,13 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.view.View.OnClickListener;
 
 import static android.content.Context.SENSOR_SERVICE;
 
 /**
  * Created by Angel on 16/3/27.
  */
-public class ShakeFragment extends Fragment implements SensorEventListener{
+public class ShakeFragment extends Fragment implements SensorEventListener,OnClickListener{
 
     private Button btnFood;
     private Button btnFilm;
@@ -30,10 +31,30 @@ public class ShakeFragment extends Fragment implements SensorEventListener{
     SensorManager sensorManager=null;
     Vibrator vibrator=null;
 
+    String type=null;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState){
 
+        //Get UI components and set onClick action.
+        View view= inflater.inflate(R.layout.fragment_shake,container,false);
+
+        btnFood= (Button) view.findViewById(R.id.btn_food);
+        btnFilm= (Button) view.findViewById(R.id.btn_film);
+        btnHotel= (Button) view.findViewById(R.id.btn_hotel);
+        btnMusic= (Button) view.findViewById(R.id.btn_music);
+        btnSports= (Button) view.findViewById(R.id.btn_music);
+        btnEntertainment= (Button) view.findViewById(R.id.btn_entertainment);
+
+        btnFood.setOnClickListener(this);
+        btnFilm.setOnClickListener(this);
+        btnHotel.setOnClickListener(this);
+        btnMusic.setOnClickListener(this);
+        btnSports.setOnClickListener(this);
+        btnEntertainment.setOnClickListener(this);
+
+        //Get Vibrate Sensor.
         sensorManager= (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
         vibrator= (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
 
@@ -82,4 +103,28 @@ public class ShakeFragment extends Fragment implements SensorEventListener{
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_food:
+                type="food";
+                break;
+            case R.id.btn_film:
+                type="film";
+                break;
+            case R.id.btn_hotel:
+                type="hotel";
+                break;
+            case R.id.btn_music:
+                type="music";
+                break;
+            case R.id.btn_sports:
+                type="sports";
+                break;
+            case R.id.btn_entertainment:
+                type="entertainments";
+                break;
+        }
+
+    }
 }
