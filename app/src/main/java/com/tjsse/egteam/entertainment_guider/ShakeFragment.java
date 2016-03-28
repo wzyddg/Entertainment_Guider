d15cc6f019162cc3a49a181304091b6754d86783
杨安琪 <yangllzz01@163.com>
2016-03-28 16:14:27 +0800

    更新“摇一摇”功能。

--- app/src/main/java/com/tjsse/egteam/entertainment_guider/ShakeFragment.java
+++ app/src/main/java/com/tjsse/egteam/entertainment_guider/ShakeFragment.java
@@ -1,6 +1,7 @@
 package com.tjsse.egteam.entertainment_guider;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,6 +11,7 @@ import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import static android.content.Context.SENSOR_SERVICE;
 
@@ -18,11 +20,23 @@ import static android.content.Context.SENSOR_SERVICE;
  */
 public class ShakeFragment extends Fragment implements SensorEventListener{
 
+    private Button btnFood;
+    private Button btnFilm;
+    private Button btnHotel;
+    private Button btnMusic;
+    private Button btnSports;
+    private Button btnEntertainment;
+
     SensorManager sensorManager=null;
     Vibrator vibrator=null;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState){
+
+        sensorManager= (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
+        vibrator= (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+
         return inflater.inflate(R.layout.fragment_shake,container,false);
     }
 
@@ -39,7 +53,9 @@ public class ShakeFragment extends Fragment implements SensorEventListener{
                     (Math.abs(values[1])>17)||
                     (Math.abs(values[2])>17))
             {
-                vibrator.vibrate(500);
+                //vibrate at 200ms, 400ms, 600ms
+                long[] pattern={100,200,300,400,500,600,700};
+                vibrator.vibrate(pattern,-1);
             }
         }
 
@@ -49,4 +65,21 @@ public class ShakeFragment extends Fragment implements SensorEventListener{
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         //当传感器精度改变时回调该方法，Do nothing.
     }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
 }
